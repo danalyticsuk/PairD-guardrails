f7ddddcd33a5835fa743c65ce43d141a69e1911b
abolton1999 <angusbolton@deloitte.co.uk>
2023-10-27 11:27:28 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+# Dependency Imports
+import fastapi
+import loguru
+
+# Initialize app:
+app = fastapi.FastAPI()
+
+# Write some health check or other function here
+
+# General Model initializations:
+
+
+
+# Add routers to the app - create a for loop
+
+from app.routes.input_ingestion import router as input_ingest
+from app.routes.input_post import router as input_post
+from app.routes.output_ingestion import router as output_ingest
+from app.routes.output_post import router as output_post
+
+for router in [input_ingest, input_post, output_ingest, output_post]:
+    app.include_router(router)
